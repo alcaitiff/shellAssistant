aa5fc7b331440dc024ba875d9610145a1022b368
Alan Rabello <alan.rabello@serpro.gov.br>
2022-01-28 10:22:09 -0300

    Added the learn on editor command

---
+++ src/usr/local/shellAssistant/learn-on-editor.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Description: Learn a new command and open the default editor to write it
+function learn-on-editor { 
+
+  . /usr/local/shellAssistant/lib
+
+	if [ -z "$1" ]; then
+		if promptyn "Learn as alias"; then
+			commandType=1;
+		else
+			commandType=0;
+		fi
+	else
+		if [ "${1}" = "alias" ]; then
+			commandType=1;
+		else
+			commandType=0;
+		fi
+	fi
+
+	if [ -z "$2" ]; then
+		name=$(askit "What should I learn?");
+	else
+		name=${2};
+	fi
+
+	if [ -z "$3" ]; then
+		command="echo 'No code yet'";
+	else
+		command=${3};
+	fi
+
+	if [ -z "$4" ]; then
+		description="No description yet";
+	else
+		description=${4};
+	fi
+
+  learn $1 $2 "${command}" "${description}" && change "${command}" && resource;
+}
